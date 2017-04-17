21a68cb9ce0df4b41aed026fc86bc3da5cd7d7ee
Frank Galligan <fgalligan@google.com>
2017-04-17 11:05:43 -0700

    Increase version to "0.10.1"
    This fixes #93

--- core/draco_version.h
+++ core/draco_version.h
@@ -18,7 +18,7 @@
 namespace draco {
 
 // Draco version is comprised of <major>.<minor>.<revision>.
-static const char kDracoVersion[] = "0.10.0";
+static const char kDracoVersion[] = "0.10.1";
 
 const char *Version() { return kDracoVersion; }
 
