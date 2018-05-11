9afc0d317720d120bb27c8525b66cb10c06a193b
Federico De Felici <fdf8210@gmail.com>
2018-05-11 11:25:56 +0200

    First version of draco wrapper for python

---
+++ maya/draco_example.py
@@ -0,0 +1,14 @@
+from draco import Draco
+
+drc = Draco()
+mesh = drc.decode('bunny.drc')
+
+print("\n==== FACES ====")
+print(mesh.faces_num)
+print(mesh.faces[0:10])
+print("\n==== VERTICES ====")
+print(mesh.vertices_num)
+print(mesh.vertices[0:10])
+print("\n==== NORMALS ====")
+print(mesh.normals_num)
+# print(mesh.normals[0:10])  This mesh has no normals
