9d02e1cd75d722b3f050a060223f808f96a0b74d
Federico De Felici <fdf8210@gmail.com>
2018-05-11 15:39:58 +0200

    Updated example script

--- maya/dracodec_example.py
+++ maya/dracodec_example.py
@@ -5,10 +5,13 @@ mesh = drc.decode('bunny.drc')
 
 print("\n==== FACES ====")
 print(mesh.faces_num)
+print(mesh.faces_len)
 print(mesh.faces[0:10])
 print("\n==== VERTICES ====")
 print(mesh.vertices_num)
+print(mesh.vertices_len)
 print(mesh.vertices[0:10])
 print("\n==== NORMALS ====")
 print(mesh.normals_num)
+print(mesh.normals_len)
 # print(mesh.normals[0:10])  This mesh has no normals
