0a89322896b2435ce77062627310b39ac382e0ee
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-07-11 01:28:05 +0200

    Improved Unity integration: - Speed-up: Bulk memory copies instead of per vertex/index data copy - Speed-up: Multi-threaded via C# Job system - Supports single meshes with more than 65536 vertices (old split algorithm was broken) - Fixed iOS/WebGL build and macOS editor by correcting DllImport parameter

--- unity/DracoMeshLoader.cs
+++ unity/DracoMeshLoader.cs
@@ -1,4 +1,4 @@
-// Copyright 2017 The Draco Authors.
+// Copyright 2019 The Draco Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,14 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// #define DRACO_VERBOSE
+
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Events;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
 
 public unsafe class DracoMeshLoader
 {
+	#if UNITY_EDITOR_OSX || UNITY_WEBGL || UNITY_IOS
+        public const string DRACODEC_UNITY_LIB = "__Internal";
+    #elif UNITY_ANDROID || UNITY_STANDALONE
+        public const string DRACODEC_UNITY_LIB = "dracodec_unity";
+    #endif
+
+	const Allocator defaultAllocator = Allocator.Persistent;
+
 	// Must stay the order to be consistent with C++ interface.
 	[StructLayout (LayoutKind.Sequential)] private struct DracoToUnityMesh
 	{
@@ -35,142 +50,26 @@ public unsafe class DracoMeshLoader
 		public IntPtr color;
 	}
 
-	private struct DecodedMesh
-	{
-		public int[] indices;
-		public Vector3[] vertices;
-		public Vector3[] normals;
-		public Vector2[] uvs;
-		public Color[] colors;
-	}
+	struct DracoJob : IJob {
 
-	[DllImport ("dracodec_unity")] private static extern int DecodeMeshForUnity (
-		byte[] buffer, int length, DracoToUnityMesh**tmpMesh);
+		[ReadOnly]
+		public NativeArray<byte> data;
 
-	static private int maxNumVerticesPerMesh = 60000;
+		public NativeArray<IntPtr> outMesh;
 
-	// Unity only support maximum 65534 vertices per mesh. So large meshes need
-	// to be splitted.
-	private void SplitMesh (DecodedMesh mesh, ref List<DecodedMesh> splittedMeshes)
-	{
-		// Map between new indices on a splitted mesh and old indices on the
-		// original mesh.
-		int[] newToOldIndexMap = new int[maxNumVerticesPerMesh];
+		public NativeArray<int> result;
 
-		// Index of the first unprocessed corner.
-		int baseCorner = 0;
-		int indicesCount = mesh.indices.Length;
-
-		// Map between old indices of the original mesh and indices on the currently
-		// processed sub-mesh. Inverse of |newToOldIndexMap|.
-		int[] oldToNewIndexMap = new int[indicesCount];
-		int[] newIndices = new int[indicesCount];
-
-
-		// Set mapping between existing vertex indices and new vertex indices to
-		// a default value.
-		for (int i = 0; i < indicesCount; i++)
-		{
-			oldToNewIndexMap[i] = -1;
-		}
-
-		// Number of added vertices for the currently processed sub-mesh.
-		int numAddedVertices = 0;
-
-		// Process all corners (faces) of the original mesh.
-		while (baseCorner < indicesCount)
-		{
-			// Reset the old to new indices map that may have been set by previously
-			// processed sub-meshes.
-			for (int i = 0; i < numAddedVertices; i++)
-			{
-				oldToNewIndexMap[newToOldIndexMap[i]] = -1;
-			}
-			numAddedVertices = 0;
-
-			// Number of processed corners on the current sub-mesh.
-			int numProcessedCorners = 0;
-
-			// Local storage for indices added to the new sub-mesh for a currently
-			// processed face.
-			int[] newlyAddedIndices = new int[3];
-
-			// Sub-mesh processing starts here.
-			for (; baseCorner + numProcessedCorners < indicesCount;)
-			{
-				// Number of vertices that we need to add to the current sub-mesh.
-				int verticesAdded = 0;
-				for (int i = 0; i < 3; i++)
-				{
-					if (oldToNewIndexMap[mesh.indices[baseCorner + numProcessedCorners + i]] == -1)
-					{
-						newlyAddedIndices[verticesAdded] = mesh.indices[baseCorner + numProcessedCorners + i];
-						verticesAdded++;
-					}
-				}
-
-				// If the number of new vertices that we need to add is larger than the
-				// allowed limit, we need to stop processing the current sub-mesh.
-				// The current face will be processed again for the next sub-mesh.
-				if (numAddedVertices + verticesAdded > maxNumVerticesPerMesh)
-				{
-					break;
-				}
-
-				// Update mapping between old an new vertex indices.
-				for (int i = 0; i < verticesAdded; i++)
-				{
-					oldToNewIndexMap[newlyAddedIndices[i]] = numAddedVertices;
-					newToOldIndexMap[numAddedVertices] = newlyAddedIndices[i];
-					numAddedVertices++;
-				}
-
-				for (int i = 0; i < 3; i++)
-				{
-					newIndices[numProcessedCorners] = oldToNewIndexMap[mesh.indices[baseCorner + numProcessedCorners]];
-					numProcessedCorners++;
-				}
-			}
-			// Sub-mesh processing done.
-			DecodedMesh subMesh = new DecodedMesh();
-			subMesh.indices = new int[numProcessedCorners];
-			Array.Copy(newIndices, subMesh.indices, numProcessedCorners);
-			subMesh.vertices = new Vector3[numAddedVertices];
-			for (int i = 0; i < numAddedVertices; i++)
-			{
-				subMesh.vertices[i] = mesh.vertices[newToOldIndexMap[i]];
-			}
-			if (mesh.normals != null)
-			{
-				subMesh.normals = new Vector3[numAddedVertices];
-				for (int i = 0; i < numAddedVertices; i++)
-				{
-					subMesh.normals[i] = mesh.normals[newToOldIndexMap[i]];
-				}
-			}
-
-			if (mesh.colors != null)
-			{
-				subMesh.colors = new Color[numAddedVertices];
-				for (int i = 0; i < numAddedVertices; i++)
-				{
-					subMesh.colors[i] = mesh.colors[newToOldIndexMap[i]];
-				}
-			}
-
-			if (mesh.uvs != null)
-			{
-				subMesh.uvs = new Vector2[numAddedVertices];
-				for (int i = 0; i < numAddedVertices; i++)
-				{
-					subMesh.uvs[i] = mesh.uvs[newToOldIndexMap[i]];
-				}
-			}
-
-			splittedMeshes.Add(subMesh);
-			baseCorner += numProcessedCorners;
+		public void Execute() {
+			DracoToUnityMesh* tmpMesh;
+			result[0] = DecodeMeshForUnity (data.GetUnsafeReadOnlyPtr(), data.Length, &tmpMesh);
+			outMesh[0] = (IntPtr) tmpMesh;
 		}
 	}
+
+	[DllImport (DRACODEC_UNITY_LIB)] private static extern int DecodeMeshForUnity (
+		void* buffer, int length, DracoToUnityMesh**tmpMesh);
+
+	[DllImport(DRACODEC_UNITY_LIB)] private static extern int ReleaseUnityMesh(DracoToUnityMesh** tmpMesh);
 
 	private float ReadFloatFromIntPtr (IntPtr data, int offset)
 	{
@@ -181,191 +80,173 @@ public unsafe class DracoMeshLoader
 		return BitConverter.ToSingle (byteArray, 0);
 	}
 
-	// TODO(zhafang): Add back LoadFromURL.
-	public int LoadMeshFromAsset (string assetName, ref List<Mesh> meshes)
-	{
-		TextAsset asset = Resources.Load (assetName, typeof(TextAsset)) as TextAsset;
-		if (asset == null) {
-			Debug.Log ("Didn't load file!");
-			return -1;
+	public UnityAction<Mesh> onMeshesLoaded;
+
+	public IEnumerator DecodeMesh(NativeArray<byte> data) {
+
+		Profiler.BeginSample("JobPrepare");
+		var job = new DracoJob();
+
+		job.data = data;
+		job.result = new NativeArray<int>(1,defaultAllocator);
+		job.outMesh = new NativeArray<IntPtr>(1,defaultAllocator);
+
+		var jobHandle = job.Schedule();
+		Profiler.EndSample();
+
+		while(!jobHandle.IsCompleted) {
+			yield return null;
 		}
-		byte[] encodedData = asset.bytes;
-		Debug.Log (encodedData.Length.ToString ());
-		if (encodedData.Length == 0) {
-			Debug.Log ("Didn't load encoded data!");
-			return -1;
+		jobHandle.Complete();
+
+		int result = job.result[0];
+		IntPtr dracoMesh = job.outMesh[0];
+
+		job.result.Dispose();
+		job.outMesh.Dispose();
+
+		if (result <= 0) {
+			Debug.LogError ("Failed: Decoding error.");
+			yield break;
 		}
-		return DecodeMesh (encodedData, ref meshes);
+
+		var mesh = CreateMesh(dracoMesh);
+
+		if(onMeshesLoaded!=null) {
+			onMeshesLoaded(mesh);
+		}
 	}
 
-	public unsafe int DecodeMesh (byte[] data, ref List<Mesh> meshes)
+#if UNITY_EDITOR
+	/// <summary>
+	/// Synchronous (non-threaded) version of DecodeMesh for Edtior purpose.
+	/// </summary>
+	/// <param name="data">Drace file data</param>
+	/// <returns>The Mesh</returns>
+	public Mesh DecodeMeshSync(NativeArray<byte> data) {
+
+		Profiler.BeginSample("JobPrepare");
+		var job = new DracoJob();
+
+		job.data = data;
+		job.result = new NativeArray<int>(1,defaultAllocator);
+		job.outMesh = new NativeArray<IntPtr>(1,defaultAllocator);
+
+		job.Run();
+		Profiler.EndSample();
+
+		int result = job.result[0];
+		IntPtr dracoMesh = job.outMesh[0];
+
+		job.result.Dispose();
+		job.outMesh.Dispose();
+
+		if (result <= 0) {
+			Debug.LogError ("Failed: Decoding error.");
+			return null;
+		}
+
+		return CreateMesh(dracoMesh);
+	}
+#endif
+
+	unsafe Mesh CreateMesh (IntPtr dracoMesh)
 	{
-		DracoToUnityMesh* tmpMesh;
-		if (DecodeMeshForUnity (data, data.Length, &tmpMesh) <= 0) {
-			Debug.Log ("Failed: Decoding error.");
-			return -1;
-		}
+		Profiler.BeginSample("CreateMesh");
+		DracoToUnityMesh* tmpMesh = (DracoToUnityMesh*) dracoMesh;
 
-		Debug.Log ("Num indices: " + tmpMesh->numFaces.ToString ());
-		Debug.Log ("Num vertices: " + tmpMesh->numVertices.ToString ());
-		if (tmpMesh->hasNormal)
-			Debug.Log ("Decoded mesh normals.");
-		if (tmpMesh->hasTexcoord)
-			Debug.Log ("Decoded mesh texcoords.");
-		if (tmpMesh->hasColor)
-			Debug.Log ("Decoded mesh colors.");
+		Log ("Num indices: " + tmpMesh->numFaces.ToString ());
+		Log ("Num vertices: " + tmpMesh->numVertices.ToString ());
 
-		int numFaces = tmpMesh->numFaces;
+		Profiler.BeginSample("CreateMeshAlloc");
 		int[] newTriangles = new int[tmpMesh->numFaces * 3];
-		for (int i = 0; i < tmpMesh->numFaces; ++i) {
-			byte* addr = (byte*)tmpMesh->indices + i * 3 * 4;
-			newTriangles[i * 3] = *((int*)addr);
-			newTriangles[i * 3 + 1] = *((int*)(addr + 4));
-			newTriangles[i * 3 + 2] = *((int*)(addr + 8));
-		}
-
-		// For floating point numbers, there's no Marshal functions could directly
-		// read from the unmanaged data.
-		// TODO(zhafang): Find better way to read float numbers.
 		Vector3[] newVertices = new Vector3[tmpMesh->numVertices];
-		Vector2[] newUVs = new Vector2[0];
-		if (tmpMesh->hasTexcoord)
-			newUVs = new Vector2[tmpMesh->numVertices];
-		Vector3[] newNormals = new Vector3[0];
-		if (tmpMesh->hasNormal)
-			newNormals = new Vector3[tmpMesh->numVertices];
-		Color[] newColors = new Color[0];
-		if (tmpMesh->hasColor)
-			newColors = new Color[tmpMesh->numVertices];
-		int byteStridePerValue = 4;
+		Profiler.EndSample();
 
+		Vector2[] newUVs = null;
+		Vector3[] newNormals = null;
+		Color[] newColors = null;
+
+		Profiler.BeginSample("CreateMeshIndices");
+		byte* indicesSrc = (byte*)tmpMesh->indices;
+		var indicesPtr = UnsafeUtility.AddressOf(ref newTriangles[0]);
+		UnsafeUtility.MemCpy(indicesPtr,indicesSrc,newTriangles.Length*4);
+		Profiler.EndSample();
+
+		Profiler.BeginSample("CreateMeshPositions");
 		byte* posaddr = (byte*)tmpMesh->position;
-		byte* normaladdr = (byte*)tmpMesh->normal;
-		byte* coloraddr = (byte*)tmpMesh->color;
-		byte* uvaddr = (byte*)tmpMesh->texcoord;
-		for (int i = 0; i < tmpMesh->numVertices; ++i)
-		{
-			int numValuePerVertex = 3;
-			for (int j = 0; j < numValuePerVertex; ++j)
-			{
-				int byteStridePerVertex = byteStridePerValue * numValuePerVertex;
-				int OffSet = i * byteStridePerVertex + byteStridePerValue * j;
+		/// TODO(atteneder): check if we can avoid mem copies with new Mesh API (2019.3?)
+		/// by converting void* to NativeArray via
+		/// NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray
+		var newVerticesPtr = UnsafeUtility.AddressOf(ref newVertices[0]);
+		UnsafeUtility.MemCpy(newVerticesPtr,posaddr,tmpMesh->numVertices * 12 );
+		Profiler.EndSample();
 
-				newVertices[i][j] = *((float*)(posaddr + OffSet));
-				if (tmpMesh->hasNormal)
-				{
-					newNormals[i][j] = *((float*)(normaladdr + OffSet));
-				}
-			}
-
-			if (tmpMesh->hasColor)
-			{
-				numValuePerVertex = 4;
-				for (int j = 0; j < numValuePerVertex; ++j)
-				{
-					int byteStridePerVertex = byteStridePerValue * numValuePerVertex;
-					newColors[i][j] = *((float*)(coloraddr + (i * byteStridePerVertex + byteStridePerValue * j)));
-				}
-			}
-
-			if (tmpMesh->hasTexcoord)
-			{
-				numValuePerVertex = 2;
-				for (int j = 0; j < numValuePerVertex; ++j)
-				{
-					int byteStridePerVertex = byteStridePerValue * numValuePerVertex;
-					newUVs[i][j] = *((float*)(uvaddr + (i * byteStridePerVertex + byteStridePerValue * j)));
-				}
-			}
+		if (tmpMesh->hasTexcoord) {
+			Profiler.BeginSample("CreateMeshUVs");
+			Log ("Decoded mesh texcoords.");
+			newUVs = new Vector2[tmpMesh->numVertices];
+			byte* uvaddr = (byte*)tmpMesh->texcoord;
+			var newUVsPtr = UnsafeUtility.AddressOf(ref newUVs[0]);
+			UnsafeUtility.MemCpy(newUVsPtr,uvaddr,tmpMesh->numVertices * 8 );
+			Profiler.EndSample();
+		}
+		if (tmpMesh->hasNormal) {
+			Profiler.BeginSample("CreateMeshNormals");
+			Log ("Decoded mesh normals.");
+			newNormals = new Vector3[tmpMesh->numVertices];
+			byte* normaladdr = (byte*)tmpMesh->normal;
+			var newNormalsPtr = UnsafeUtility.AddressOf(ref newNormals[0]);
+			UnsafeUtility.MemCpy(newNormalsPtr,normaladdr,tmpMesh->numVertices * 12 );
+			Profiler.EndSample();
+		}
+		if (tmpMesh->hasColor) {
+			Profiler.BeginSample("CreateMeshColors");
+			Log ("Decoded mesh colors.");
+			newColors = new Color[tmpMesh->numVertices];
+			byte* coloraddr = (byte*)tmpMesh->color;
+			var newColorsPtr = UnsafeUtility.AddressOf(ref newColors[0]);
+			UnsafeUtility.MemCpy(newColorsPtr,coloraddr,tmpMesh->numVertices * 16 );
+			Profiler.EndSample();
 		}
 
+		Profiler.BeginSample("CreateMeshRelease");
 		ReleaseUnityMesh (&tmpMesh);
+		Profiler.EndSample();
 
-		if (newVertices.Length > maxNumVerticesPerMesh) {
-			// Unity only support maximum 65534 vertices per mesh. So large meshes
-			// need to be splitted.
+		Profiler.BeginSample("CreateMeshFeeding");
+		Mesh mesh = new Mesh ();
+#if UNITY_2017_3_OR_NEWER
+		mesh.indexFormat =  (newVertices.Length > System.UInt16.MaxValue)
+		? UnityEngine.Rendering.IndexFormat.UInt32
+		: UnityEngine.Rendering.IndexFormat.UInt16;
+#else
+		if(newVertices.Length > System.UInt16.MaxValue) {
+			throw new System.Exception("Draco meshes with more than 65535 vertices are only supported from Unity 2017.3 onwards.");
+		}
+#endif
 
-			DecodedMesh decodedMesh = new DecodedMesh ();
-			decodedMesh.vertices = newVertices;
-			decodedMesh.indices = newTriangles;
-			if (newUVs.Length != 0)
-				decodedMesh.uvs = newUVs;
-			if (newNormals.Length != 0)
-				decodedMesh.normals = newNormals;
-			if (newColors.Length != 0)
-				decodedMesh.colors = newColors;
-			List<DecodedMesh> splittedMeshes = new List<DecodedMesh> ();
-
-			SplitMesh (decodedMesh, ref splittedMeshes);
-			for (int i = 0; i < splittedMeshes.Count; ++i) {
-				Mesh mesh = new Mesh ();
-				mesh.vertices = splittedMeshes [i].vertices;
-				mesh.triangles = splittedMeshes [i].indices;
-				if (splittedMeshes [i].uvs != null)
-					mesh.uv = splittedMeshes [i].uvs;
-
-				if (splittedMeshes [i].colors != null) {
-					mesh.colors = splittedMeshes[i].colors;
-				}
-
-				if (splittedMeshes [i].normals != null) {
-					mesh.normals = splittedMeshes [i].normals;
-				} else {
-					Debug.Log ("Sub mesh doesn't have normals, recomputed.");
-					mesh.RecalculateNormals ();
-				}
-				mesh.RecalculateBounds ();
-				meshes.Add (mesh);
-			}
+		mesh.vertices = newVertices;
+		mesh.SetTriangles(newTriangles,0,true);
+		if (newUVs!=null) {
+			mesh.uv = newUVs;
+		}
+		if (newNormals!=null) {
+			mesh.normals = newNormals;
 		} else {
-			Mesh mesh = new Mesh ();
-			mesh.vertices = newVertices;
-			mesh.triangles = newTriangles;
-			if (newUVs.Length != 0)
-				mesh.uv = newUVs;
-			if (newNormals.Length != 0) {
-				mesh.normals = newNormals;
-			} else {
-				mesh.RecalculateNormals ();
-				Debug.Log ("Mesh doesn't have normals, recomputed.");
-			}
-			if (newColors.Length != 0) {
-				mesh.colors = newColors;
-			}
-
-			// Scale and translate the decoded mesh so it would be visible to
-			// a new camera's default settings.
-			float scale = 0.5f / mesh.bounds.extents.x;
-			if (0.5f / mesh.bounds.extents.y < scale)
-				scale = 0.5f / mesh.bounds.extents.y;
-			if (0.5f / mesh.bounds.extents.z < scale)
-				scale = 0.5f / mesh.bounds.extents.z;
-
-			Vector3[] vertices = mesh.vertices;
-			int i = 0;
-			while (i < vertices.Length) {
-				vertices[i] *= scale;
-				i++;
-			}
-
-			mesh.vertices = vertices;
-			mesh.RecalculateBounds ();
-
-			Vector3 translate = mesh.bounds.center;
-			translate.x = 0 - mesh.bounds.center.x;
-			translate.y = 0 - mesh.bounds.center.y;
-			translate.z = 2 - mesh.bounds.center.z;
-
-			i = 0;
-			while (i < vertices.Length) {
-				vertices[i] += translate;
-				i++;
-			}
-			mesh.vertices = vertices;
-			mesh.RecalculateBounds ();
-			meshes.Add (mesh);
+			mesh.RecalculateNormals ();
+			Log ("Mesh doesn't have normals, recomputed.");
+		}
+		if (newColors!=null) {
+			mesh.colors = newColors;
 		}
 
-		return numFaces;
+		Profiler.EndSample();
+		return mesh;
+	}
+
+	[System.Diagnostics.Conditional("DRACO_VERBOSE")]
+	static void Log(string format, params object[] args) {
+		Debug.LogFormat(format,args);
 	}
 }
--- unity/Editor/DracoFileImporter.cs
+++ unity/Editor/DracoFileImporter.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2017 The Draco Authors.
+﻿// Copyright 2019 The Draco Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,58 +12,65 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
 using UnityEditor;
 using UnityEngine;
+using Unity.Collections;
 
 public class DracoFileImporter : AssetPostprocessor {
+
+  const string FILE_EXT = ".drc";
+  const string FILE_EXT_BYTES = ".drc.bytes";
+
   static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
       string[] movedAssets, string[] movedFromAssetPaths) {
     foreach(string str in importedAssets) {
       // Compressed file must be renamed to ".drc.bytes".
-      if (str.IndexOf(".drc.bytes") == -1) {
+      var extIndex = str.EndsWith(FILE_EXT,true,CultureInfo.InvariantCulture);
+      var extBytesIndex = str.EndsWith(FILE_EXT_BYTES,true,CultureInfo.InvariantCulture);
+
+      if ( !(extIndex||extBytesIndex) ) {
         return;
       }
 
-      // If the original mesh exceeds the limit of number of verices, the
-      // loader will split it to a list of smaller meshes.
-      List<Mesh> meshes = new List<Mesh>();
       DracoMeshLoader dracoLoader = new DracoMeshLoader();
 
-      // The decoded mesh will be named without ".drc.bytes"
-      str.LastIndexOf('/');
-      int length = str.Length - ".drc.bytes".Length - str.LastIndexOf('/') - 1;
+      int length = str.Length - (extIndex?4:6) - str.LastIndexOf('/') - 1;
       string fileName = str.Substring(str.LastIndexOf('/') + 1, length);
 
-      int numFaces = dracoLoader.LoadMeshFromAsset(fileName + ".drc", ref meshes);
-      if (numFaces > 0) {
+      var data = File.ReadAllBytes(str);
+      var nativeData = new NativeArray<byte>(data.Length,Allocator.TempJob);
+      nativeData.CopyFrom(data);
+      var mesh = dracoLoader.DecodeMeshSync(nativeData);
+      nativeData.Dispose();
+
+      if (mesh!=null) {
+        mesh.name = fileName;
         // Create mesh assets. Combine the smaller meshes to a single asset.
         // TODO: Figure out how to combine to an unseen object as .obj files.
-        AssetDatabase.CreateAsset (meshes [0], "Assets/Resources/" + fileName + ".asset");
+        var dir = Path.GetDirectoryName(str);
+        AssetDatabase.CreateAsset (mesh, Path.Combine(dir, fileName + ".asset" ));
         AssetDatabase.SaveAssets ();
-        for (int i = 1; i < meshes.Count; ++i) {
-          AssetDatabase.AddObjectToAsset(meshes [i], meshes [0]);
-          AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath (meshes [i]));
-        }
-
+        
         // Also create a Prefab for easy usage.
         GameObject newAsset = new GameObject();
         newAsset.hideFlags = HideFlags.HideInHierarchy;
-        for (int i = 0; i < meshes.Count; ++i) {
-          GameObject subObject = new GameObject();
-          subObject.hideFlags = HideFlags.HideInHierarchy;
-          subObject.AddComponent<MeshFilter>();
-          subObject.AddComponent<MeshRenderer>();
-          subObject.GetComponent<MeshFilter>().mesh =
-            UnityEngine.Object.Instantiate(meshes[i]);
-          subObject.transform.parent = newAsset.transform;
+        var mf = newAsset.AddComponent<MeshFilter>();
+        mf.mesh = mesh;
+        newAsset.AddComponent<MeshRenderer>();
+        
+        bool success;
+        PrefabUtility.SaveAsPrefabAsset(newAsset, Path.Combine(dir, fileName + ".prefab"), out success);
+
+        Object.DestroyImmediate(newAsset);
+
+        if(!success) {
+          Debug.LogError("Creating Draco Prefab failed!");
         }
-        PrefabUtility.CreatePrefab("Assets/Resources/" + fileName + ".prefab", newAsset);
       } else {
         // TODO: Throw exception?
-        Debug.Log("Error: Decodeing Draco file failed.");
+        Debug.LogError("Decodeing Draco file failed.");
       }
     }
   }
