931bafe5781ffdc38dd31939c3decdddb700bfdd
Tom Finegan <tomfinegan@google.com>
2021-12-06 09:58:05 -0800

    draco: Improve installation and packaging support.
    - Fixed omission of draco_version.h when installed in static
      configurations.
    - Remove DracoConfig.cmake: it was broken and we actually use
      draco-config.cmake.
    - Update FindDraco.cmake so it actually has a chance of working
      properly. Fixed usage of find_path() and subsequent related errors.
    - Correct the usage of CMakePackageConfigHelpers in installation target setup
      and the config template.
    - Add a CMake version file.
    - Normalize the Draco variables exposed in CMake.
      - draco_FOUND -> DRACO_FOUND
      - DRACO_INCLUDE_DIRS, draco_INCLUDE_DIRS -> DRACO_INCLUDE_DIR
      - DRACO_LIBRARIES, draco_LIBRARIES -> DRACO_LIBRARY
      - DRACO_LIBRARY_DIRS, draco_LIBRARY_DIRS -> DRACO_LIBRARY_DIR
      - draco_VERSION_STRING -> DRACO_VERSION
    - Use full path variants of GNUInstallDirs variables to init our
      install paths.
    
    Fixes https://github.com/google/draco/issues/764
    
    * Add install() for draco-version.cmake.

--- cmake/draco_install.cmake
+++ cmake/draco_install.cmake
@@ -17,30 +17,41 @@ if(DRACO_CMAKE_DRACO_INSTALL_CMAKE_)
 endif() # DRACO_CMAKE_DRACO_INSTALL_CMAKE_
 set(DRACO_CMAKE_DRACO_INSTALL_CMAKE_ 1)
 
+include(CMakePackageConfigHelpers)
+include(GNUInstallDirs)
+
 # Sets up the draco install targets. Must be called after the static library
 # target is created.
 macro(draco_setup_install_target)
-  include(GNUInstallDirs)
-
-  # pkg-config: draco.pc
-  set(prefix "${CMAKE_INSTALL_PREFIX}")
-  set(exec_prefix "\${prefix}")
-  set(libdir "\${prefix}/${CMAKE_INSTALL_LIBDIR}")
-  set(includedir "\${prefix}/${CMAKE_INSTALL_INCLUDEDIR}")
+  set(bin_path "${CMAKE_INSTALL_FULL_BINDIR}")
+  set(data_path "${CMAKE_INSTALL_FULL_DATAROOTDIR}")
+  set(includes_path "${CMAKE_INSTALL_FULL_INCLUDEDIR}")
+  set(libs_path "${CMAKE_INSTALL_FULL_LIBDIR}")
   set(draco_lib_name "draco")
 
+  # pkg-config: draco.pc
   configure_file("${draco_root}/cmake/draco.pc.template"
                  "${draco_build}/draco.pc" @ONLY NEWLINE_STYLE UNIX)
-  install(FILES "${draco_build}/draco.pc"
-          DESTINATION "${prefix}/${CMAKE_INSTALL_LIBDIR}/pkgconfig")
+  install(FILES "${draco_build}/draco.pc" DESTINATION "${libs_path}/pkgconfig")
 
   # CMake config: draco-config.cmake
-  set(DRACO_INCLUDE_DIRS "${prefix}/${CMAKE_INSTALL_INCLUDEDIR}")
-  configure_file("${draco_root}/cmake/draco-config.cmake.template"
-                 "${draco_build}/draco-config.cmake" @ONLY NEWLINE_STYLE UNIX)
+  set(DRACO_INCLUDE_DIR "${includes_path}")
+  configure_package_config_file(
+    "${draco_root}/cmake/draco-config.cmake.template"
+    "${draco_build}/draco-config.cmake"
+    INSTALL_DESTINATION "${data_path}/cmake")
   install(
     FILES "${draco_build}/draco-config.cmake"
-    DESTINATION "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_DATAROOTDIR}/cmake")
+    DESTINATION "${data_path}/cmake")
+
+  # CMake version: draco-version.cmake
+  write_basic_package_version_file(
+    "${draco_build}/draco-version.cmake"
+    VERSION ${DRACO_VERSION}
+    COMPATIBILITY AnyNewerVersion)
+  install(
+      FILES "${draco_build}/draco-version.cmake"
+      DESTINATION "${data_path}/cmake")
 
   foreach(file ${draco_sources})
     if(file MATCHES "h$")
@@ -48,46 +59,42 @@ macro(draco_setup_install_target)
     endif()
   endforeach()
 
+  list(REMOVE_DUPLICATES draco_api_includes)
+
   # Strip $draco_src_root from the file paths: we need to install relative to
   # $include_directory.
   list(TRANSFORM draco_api_includes REPLACE "${draco_src_root}/" "")
-  set(include_directory "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}")
 
   foreach(draco_api_include ${draco_api_includes})
     get_filename_component(file_directory ${draco_api_include} DIRECTORY)
-    set(target_directory "${include_directory}/draco/${file_directory}")
+    set(target_directory "${includes_path}/draco/${file_directory}")
     install(FILES ${draco_src_root}/${draco_api_include}
             DESTINATION "${target_directory}")
   endforeach()
 
   install(
     FILES "${draco_build}/draco/draco_features.h"
-    DESTINATION "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}/draco/")
+    DESTINATION "${includes_path}/draco/")
 
-  install(TARGETS draco_decoder DESTINATION
-                  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
-  install(TARGETS draco_encoder DESTINATION
-                  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
+  install(TARGETS draco_decoder DESTINATION "${bin_path}")
+  install(TARGETS draco_encoder DESTINATION "${bin_path}")
 
   if(MSVC)
-    install(TARGETS draco DESTINATION
-                    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+    install(TARGETS draco DESTINATION "${libs_path}")
   else()
-    install(TARGETS draco_static DESTINATION
-                    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+    install(TARGETS draco_static DESTINATION "${libs_path}")
+
     if(BUILD_SHARED_LIBS)
-      install(TARGETS draco_shared DESTINATION
-                      "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+      install(TARGETS draco_shared DESTINATION "${libs_path}")
     endif()
   endif()
 
   if(DRACO_UNITY_PLUGIN)
-    install(TARGETS dracodec_unity DESTINATION
-                    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+    install(TARGETS dracodec_unity DESTINATION "${libs_path}")
   endif()
+
   if(DRACO_MAYA_PLUGIN)
-    install(TARGETS draco_maya_wrapper DESTINATION
-                    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+    install(TARGETS draco_maya_wrapper DESTINATION "${libs_path}")
   endif()
 
 endmacro()
