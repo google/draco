18f3c10df84afbd96702bdffbe880324fb8718b7
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-04-08 21:47:30 +0200

    fix install destination of CMake config files

--- cmake/draco_install.cmake
+++ cmake/draco_install.cmake
@@ -24,7 +24,6 @@ include(GNUInstallDirs)
 # target is created.
 macro(draco_setup_install_target)
   set(bin_path "${CMAKE_INSTALL_FULL_BINDIR}")
-  set(data_path "${CMAKE_INSTALL_FULL_DATAROOTDIR}")
   set(includes_path "${CMAKE_INSTALL_FULL_INCLUDEDIR}")
   set(libs_path "${CMAKE_INSTALL_FULL_LIBDIR}")
   set(draco_lib_name "draco")
@@ -39,10 +38,10 @@ macro(draco_setup_install_target)
   configure_package_config_file(
     "${draco_root}/cmake/draco-config.cmake.template"
     "${draco_build}/draco-config.cmake"
-    INSTALL_DESTINATION "${data_path}/cmake")
+    INSTALL_DESTINATION "${libs_path}/cmake/draco")
   install(
     FILES "${draco_build}/draco-config.cmake"
-    DESTINATION "${data_path}/cmake")
+    DESTINATION "${libs_path}/cmake/draco")
 
   # CMake version: draco-version.cmake
   write_basic_package_version_file(
@@ -51,7 +50,7 @@ macro(draco_setup_install_target)
     COMPATIBILITY AnyNewerVersion)
   install(
       FILES "${draco_build}/draco-version.cmake"
-      DESTINATION "${data_path}/cmake")
+      DESTINATION "${libs_path}/cmake/draco")
 
   foreach(file ${draco_sources})
     if(file MATCHES "h$")
