8870e2338e4186c3f0f9be9918d4a5234a0e01b5
Andreas Atteneder <andreas.atteneder@unity3d.com>
2023-11-02 17:08:05 +0100

    Added Unity WebGL sub-packages (#3)
    * feat: Initial version of the WebGL packages
    * feat(ci): Yamato CI scripts
    * feat: Unit test that sanity checks the native library file sizes

---
+++ UnityPackages/com.unity.cloud.draco.webgl-2021/Documentation~/api_index.md
@@ -0,0 +1,17 @@
+---
+uid: api-index
+---
+
+# Draco for Unity WebGL Sub-Package API Documentation
+
+This package does not provide any public API. Instead please consult the documentation of its main package [Draco for Unity][DracoForUnity].
+
+## Trademarks
+
+*Unity&reg;* is a registered trademark of [Unity Technologies][Unity].
+
+*Draco&trade;* is a trademark of [*Google LLC*][GoogleLLC].
+
+[DracoForUnity]: https://docs.unity3d.com/Packages/com.unity.cloud.draco@latest
+[GoogleLLC]: https://about.google/
+[unity]: https://unity.com
---
+++ UnityPackages/com.unity.cloud.draco.webgl-2022/Editor/Scripts/SubPackageCleanup.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Linq;
+
+namespace SubPackage
+{
+    static class SubPackageCleanup
+    {
+
+        const string k_Name = "com.unity.cloud.draco.webgl-2022";
+        const string k_DisplayName = "Draco for Unity WebGL 2022";
+
+        static readonly string[] k_MainPackageNames = {
+            "com.unity.cloud.draco",
+            "com.atteneder.draco"
+        };
+
+#if !DISABLE_SUB_PACKAGE_CHECK
+        [InitializeOnLoadMethod]
+#endif
+        static async void InitializeOnLoad()
+        {
+            var request = Client.List(offlineMode: true, includeIndirectDependencies: false);
+
+            while (!request.IsCompleted)
+                await Task.Yield();
+
+            Assert.AreEqual(StatusCode.Success, request.Status);
+
+            var mainPackageInManifest = request.Result.Select(package => package.name).Intersect(k_MainPackageNames).Any();
+
+            if (!mainPackageInManifest)
+            {
+                Debug.LogWarning($"Package {k_DisplayName} ({k_Name}) is missing its main package <a href=\"https://docs.unity3d.com/Packages/com.unity.cloud.draco@latest/\">Draco for Unity</a> and should get removed.");
+            }
+        }
+    }
+}
